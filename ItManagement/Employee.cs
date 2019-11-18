47ff90e48d8e97173fc4ca9404dfdfb55ffbecc9
Jesper Voldby Hansen <jesp58c0@edu.easj.dk>
2019-11-18 13:29:37 +0100

    Ansatte superclass oprettet

---
+++ ItManagement/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItManagement
+{
+    public class Employee
+    {
+        private int _cpr;
+        private string _userName;
+        private string _password;
+        private string _name;
+        private bool _isAdmin;
+
+        public Employee(int cpr, string userName, string password, string name, bool isAdmin)
+        {
+            _cpr = cpr;
+            _userName = userName;
+            _password = password;
+            _name = name;
+            _isAdmin = isAdmin;
+        }
+
+        public int CPR { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+
+    
+}
---
+++ ItManagement/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItManagement
+{
+    class Teacher : Employee
+    {
+        public Teacher(int cpr, string userName, string password, string name, bool isAdmin)
+            : base(cpr, userName, password, name, isAdmin)
+        {
+            
+        }
+
+        public int TCPR { get; set; }
+
+        public string TUserName { get; set; }
+
+        public string TPassword { get; set; }
+
+        public string TName { get; set; }
+
+        public bool TIsAdmin { get; set; }
+    }
+}
