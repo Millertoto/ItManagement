6ab28ffd53e01341abffd5f559362c5aa39d04a6
David N. Miller <48084926+Millertoto@users.noreply.github.com>
2019-11-18 13:08:01 +0100

    errors
    instans felt og get, set

---
+++ ItManagement/Errors.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItManagement
+{
+    class Errors
+    {
+        private int _fid;
+        private string _error;
+        private DateTime _created;
+        private DateTime _edited;
+        private bool _isRepaired;
+
+        public int Fid
+        {
+            get { return _fid;}
+            set { _fid = value; }
+
+        }
+
+        public string Error
+        {
+            get { return _error;}
+            set { _error = value; }
+
+        }
+        public DateTime Created
+        {
+            get { return _created;}
+            set { _created = value; }
+
+        }
+        public DateTime Edited
+        {
+            get { return _edited;}
+            set { _edited = value; }
+
+        }
+        public bool isRepaired
+        {
+            get { return _isRepaired;}
+            set { _isRepaired = value; }
+
+        }
+
+    }
+}
