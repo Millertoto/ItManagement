4813cb37f263c94255f4fa5a0580b8798d49ae73
MartinHolm <36170320+martinholm@users.noreply.github.com>
2019-12-02 10:43:52 +0100

    Create WebApiEmployee.cs

---
+++ ItManagement/Folder/WebApiEmployee.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ItManagement.Folder
+{
+    class WebApiEmployee
+    {
+
+        private const string ServerUrl = "http://localhost:52667";
+
+        public static List<Employee> GetEmployees(string url)
+        {
+            HttpClientHandler handler = new HttpClientHandler() { UseDefaultCredentials = true };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var status = response.Content.ReadAsStringAsync().Result;
+                        var employeeslist = JsonConvert.DeserializeObject<List<Employee>>(status);
+                        return employeeslist;
+                    }
+
+                    return new List<Employee>();
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+
+
+        public static async Task<string> PostEmployee(string url, Employee objectToPost)
+        {
+            HttpClientHandler handler = new HttpClientHandler() { UseDefaultCredentials = true };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var serializedString = JsonConvert.SerializeObject(objectToPost);
+                    StringContent content = new StringContent(serializedString, Encoding.UTF8, "application/json");
+                    HttpResponseMessage responseMessage = await client.PostAsync(url, content);
+
+                    if (responseMessage.IsSuccessStatusCode)
+                        return await responseMessage.Content.ReadAsStringAsync();
+
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+
+        }
+
+
+        public static async Task DeleteEmployee(string url)
+        {
+            HttpClientHandler handler = new HttpClientHandler() { UseDefaultCredentials = true };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    HttpResponseMessage responseMessage = await client.DeleteAsync(url);
+                    if (responseMessage.IsSuccessStatusCode)
+                        await responseMessage.Content.ReadAsStringAsync();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+        }
+
+        public async static Task PutEmployee(string url, Employee objectToPut)
+        {
+            HttpClientHandler handler = new HttpClientHandler() { UseDefaultCredentials = true };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    var serializedString = JsonConvert.SerializeObject(objectToPut);
+                    StringContent content = new StringContent(serializedString, Encoding.UTF8, "application/json");
+                    HttpResponseMessage responseMessage = await client.PutAsync(url, content);
+                    if (responseMessage.IsSuccessStatusCode) await responseMessage.Content.ReadAsStringAsync();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+        }
+
+
+    }
+}
+
