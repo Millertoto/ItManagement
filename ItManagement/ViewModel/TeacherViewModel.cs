a60fc42d4368444a404f596971907b54a25f76d4
MartinHolm <36170320+martinholm@users.noreply.github.com>
2019-12-05 11:11:03 +0100

    Tilføje + list af employees

--- ItManagement/ViewModel/TeacherViewModel.cs
+++ ItManagement/ViewModel/TeacherViewModel.cs
@@ -6,63 +6,179 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using ItManagement.Commands;
+using ItManagement.Persistencies;
 
 namespace ItManagement.ViewModel
 {
-    /*public class TeacherViewModel : INotifyPropertyChanged
+    public class TeacherViewModel : INotifyPropertyChanged
     {
-        private Teacher _domainObjectTe;
+        private string _name;
+        private string _password;
+        private string _username;
+        private int _cpr;
+        private bool _isAdmin;
+        private RelayCommand _addEmployeeButton;
+        private RelayCommand _getEmployeeList;
+        private List<Employee> _employees;
+
 
         public TeacherViewModel()
         {
-            _domainObjectTe = new Teacher(88465348, "", "", "", false);
+            _addEmployeeButton = new RelayCommand(AddEmployeeMethod);
+            _getEmployeeList = new RelayCommand(GetEmployeeList);
         }
 
-        public int TeCPR
+        public int CPR
         {
-            get { return _domainObjectTe.CPR; }
+            get { return _cpr; }
             set
             {
-                _domainObjectTe.CPR = value;
+                _cpr = value;
                 OnPropertyChanged();
             }
         }
 
-            public string TeUserName
+            public string Username
         {
-            get { return _domainObjectTe.UserName; }
+            get { return _username; }
             set
             {
-                _domainObjectTe.UserName = value;
+                _username = value;
                 OnPropertyChanged();
             }
         }
 
-        public string TePassword
+        public string Password
         {
-            get { return _domainObjectTe.Password; }
+            get { return _password; }
             set
             {
-                _domainObjectTe.Password = value;
+                _password = value;
                 OnPropertyChanged();
             }
         }
 
-        public string TeName
+        public string Name
         {
-            get { return _domainObjectTe.Name; }
+            get { return _name; }
             set
             {
-                _domainObjectTe.Name = value;
+                _name = value;
                 OnPropertyChanged();
             }
         }
 
-        public bool TeIsAdmin
+        public bool IsAdmin
         {
-            get { return _domainObjectTe.IsAdmin; }
+            get { return _isAdmin; }
+            set { _isAdmin = value; }
+        }
+
+        public RelayCommand AddEmployeeButton
+        {
+            get { return _addEmployeeButton; }
+            set { _addEmployeeButton = value; }
+
+        }
+
+        public List<Employee> Employees
+        {
+            get { return _employees; }
+            set { _employees = value; }
+
+        }
+
+        public async void GetEmployeeList()
+        {
+            Employees = WebApiEmployee.GetEmployees("api/Employees/");
+        }
+
+        public RelayCommand GetEmployeeCommand
+        {
+            get { return _getEmployeeList; }
+            set { _getEmployeeList = value; }
+
+        }
+        public async void AddEmployeeMethod()
+        {
             
+            GetEmployeeList();
+            if (UsernameCheck(Username, Employees)
+                && CprCheck(CPR, Employees)
+                && PasswordCheck(Password)
+                && NameCheck(Name))
+            {
+                Employee Emp = new Employee(Username, CPR, Password, Name, true);
+                await WebApiEmployee.PostEmployee("api/Employees/", Emp);
             }
+
+        }
+
+        public bool UsernameCheck(string username, List<Employee> list)
+        {
+            bool c = false;
+            if (username.Length >= 8 && username.Length <= 16)
+            {
+                c = true;
+                foreach (Employee e in list)
+                {
+                    if (username == e.Username)
+                    {
+                        c = false;
+                        break;
+                    }
+                }
+            }
+
+            return c;
+
+        }
+
+        public bool CprCheck(int cpr, List<Employee> list)
+        {
+            bool c = false;
+            if (cpr.ToString().Length == 10)
+            {
+                c = true;
+                foreach (Employee e in list)
+                {
+                    if (cpr == e.Cpr)
+                    {
+                        c = false;
+                        break;
+                    }
+                }
+            }
+
+            return c;
+        }
+
+        public bool PasswordCheck(string password)
+        {
+
+            if (password.Length >= 8 && password.Length <= 16)
+            {
+                return true;
+            }
+            return false;
+
+
+        }
+
+        public bool NameCheck(string name)
+        {
+            if (name.Length <= 50)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+
         
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged
@@ -71,5 +187,5 @@ namespace ItManagement.ViewModel
             PropertyChanged?.Invoke(this, new
                 PropertyChangedEventArgs(propertyName));
         }
-    }*/
+    }
 }
