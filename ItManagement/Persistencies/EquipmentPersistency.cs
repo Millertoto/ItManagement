08058fe898502a9408d0bb4b04a7f481cb4b7769
Jesper Voldby Hansen <jesp58c0@edu.easj.dk>
2019-12-05 13:51:23 +0100

    Har lavet Accessory og Equipment persistencies

---
+++ ItManagement/Persistencies/EquipmentPersistency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItManagement.Persistencies
+{
+    class EquipmentPersistency
+    {
+        const string _serverURL = "http://localhost:52667";
+        const string _equipmentURI = "Equipments";
+        const string _apiPrefix = "api";
+
+        private WebAPIAsync<Equipment> _webApi;
+
+        public EquipmentPersistency()
+        {
+            _webApi = new WebAPIAsync<Equipment>(_serverURL, _apiPrefix, _equipmentURI);
+        }
+
+        public Task<List<Equipment>> GetEquipment()
+        {
+            return _webApi.Load();
+        }
+
+        public async Task<Equipment> GetEquipment(int key)
+        {
+            return await _webApi.Read(key);
+        }
+
+        public async Task DeleteEquipment(int key)
+        {
+            await _webApi.Delete(key);
+        }
+
+        public async Task UpdateEquipment(int key, Equipment obj)
+        {
+            await _webApi.Update(key, obj);
+        }
+
+        public async Task CreateEquipment(Equipment obj)
+        {
+            await _webApi.Create(obj);
+        }
+    }
+}
