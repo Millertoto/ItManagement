0a94535132257b2dc10e22f613440aeac6987f7d
David N. Miller <48084926+Millertoto@users.noreply.github.com>
2019-12-17 14:11:45 +0100

    display selected in textbox

--- ItManagement/Models/Error.cs
+++ ItManagement/Models/Error.cs
@@ -18,7 +18,7 @@ namespace ItManagement
         #endregion
 
         #region Constructor
-
+        public Error() { }
 
         public Error(int cpr, int uid, string errorMessage, bool isRepaired, int hasRepaired)
         {
